92aa0bb4ad7e1084267a3dc1f2164beb33fc8129
SunilRajoli <rajolisunilkumar@gmail.com>
2024-07-06 12:59:11 +0530

    commit

--- prisma/migrations/20240706062039_init/migration.sql
+++ prisma/migrations/20240706062039_init/migration.sql
@@ -12,3 +12,8 @@ CREATE TABLE `Referral` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO `referrals` (`yourName`, `yourEmail`, `yourPhone`, `refereeName`, `refereeEmail`, `refereePhone`, `courseInterested`)
+VALUES
+  ('John Doe', 'john.doe@example.com', '+1234567890', 'Jane Smith', 'jane.smith@example.com', '+9876543210', 'Web Development'),
+  ('Alice Johnson', 'alice.johnson@example.com', '+1122334455', 'Bob Brown', 'bob.brown@example.com', '+9988776655', 'Data Science');
